20415b50af597c476a524362779efbcf30a0e37e
Erik Sargazakov <deftsly@outlook.com>
2021-06-03 22:22:19 +0300

    DCL-8: Generate and publish sources and doc jars

--- src/main/java/com/github/decaland/touchstone/loadout/layers/flavors/JavaLayer.java
+++ src/main/java/com/github/decaland/touchstone/loadout/layers/flavors/JavaLayer.java
@@ -8,17 +8,27 @@ import org.gradle.api.Project;
 import org.gradle.api.plugins.ApplicationPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.Copy;
-import org.gradle.api.tasks.bundling.Jar;
+import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.api.tasks.testing.Test;
+import org.gradle.jvm.tasks.Jar;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.Path;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.github.decaland.touchstone.configs.BuildParametersManifest.*;
 
 public class JavaLayer extends ProjectAwareLayer {
+
+    private static final String LOMBOK_CONFIG_TASK_NAME = "generateLombokConfig";
 
     public JavaLayer() {
     }
@@ -30,51 +40,75 @@ public class JavaLayer extends ProjectAwareLayer {
 
     @Override
     public void configure(Project project, Loadout.Layers layers) {
-        configureJavaPluginExtension(project);
-        project.getTasks().withType(JavaCompile.class, this::configureJavaPluginCompileTasks);
-        project.getTasks().withType(Test.class, this::configureJavaPluginTestCompileTasks);
-        project.getTasks().withType(Jar.class, task -> configureJavaPluginJarTasks(project, task));
-        createTaskForDownloadingDependencies(project);
+        configureExtension(requireExtension(project, JavaPluginExtension.class));
+        configureTasks(project.getTasks());
         addDependencies(project);
     }
 
-    private void configureJavaPluginExtension(@NotNull Project project) {
-        JavaPluginExtension javaPluginExtension = requireExtension(project, JavaPluginExtension.class);
+    private void configureExtension(@NotNull JavaPluginExtension extension) {
         JavaVersion javaVersion = JavaVersion.toVersion(VERSION_JAVA);
-        javaPluginExtension.setSourceCompatibility(javaVersion);
-        javaPluginExtension.setTargetCompatibility(javaVersion);
+        extension.setSourceCompatibility(javaVersion);
+        extension.setTargetCompatibility(javaVersion);
+        extension.withSourcesJar();
+        extension.withJavadocJar();
     }
 
-    private void configureJavaPluginCompileTasks(@NotNull JavaCompile task) {
-        task.getOptions().setEncoding(SOURCE_ENCODING);
+    private void configureTasks(@NotNull TaskContainer tasks) {
+        tasks.withType(JavaCompile.class, task -> task.getOptions().setEncoding(SOURCE_ENCODING));
+        tasks.withType(Test.class, task -> task.getSystemProperties().put("file.encoding", SOURCE_ENCODING));
+        tasks.withType(Jar.class, this::configureJarTasks);
+        createTaskForDownloadingDependencies(tasks);
     }
 
-    private void configureJavaPluginTestCompileTasks(@NotNull Test task) {
-        task.getSystemProperties().put("file.encoding", SOURCE_ENCODING);
-    }
-
-    private void configureJavaPluginJarTasks(@NotNull Project project, @NotNull Jar task) {
+    private void configureJarTasks(@NotNull Jar task) {
         task.getArchiveFileName().set(
-                project.provider(
-                        () -> String.format("%s.jar", project.getName())
-                )
+                task.getProject().provider(() -> generateJarName(task))
         );
     }
 
-    private void createTaskForDownloadingDependencies(@NotNull Project project) {
-        project.getTasks().register(TASK_DOWNLOAD_DEPENDENCIES, Copy.class, copyTask -> {
-            project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets().forEach(sourceSet -> {
-                copyTask.from(sourceSet.getRuntimeClasspath());
-            });
-            String tempDir = String.format("/tmp/%s", UUID.randomUUID());
+    private String generateJarName(@NotNull Jar task) {
+        String generatedName = Stream.of(
+                task.getArchiveBaseName(),
+                task.getArchiveAppendix(),
+                task.getArchiveClassifier()
+        )
+                .map(Provider::getOrNull)
+                .filter(Objects::nonNull)
+                .map(String::trim)
+                .filter(chunk -> !chunk.isBlank())
+                .collect(Collectors.joining("-"));
+        String name = generatedName.isEmpty()
+                ? task.getProject().getName().toLowerCase(Locale.ROOT)
+                : generatedName;
+        return Optional.ofNullable(task.getArchiveExtension().getOrNull())
+                .map(String::trim)
+                .filter(extension -> !extension.isBlank())
+                .map(extension -> String.format("%s.%s", name, extension))
+                .orElse(name);
+    }
+
+    private void createTaskForDownloadingDependencies(@NotNull TaskContainer tasks) {
+        tasks.register(TASK_DOWNLOAD_DEPENDENCIES, Copy.class, copyTask -> {
+            copyTask.getProject()
+                    .getConvention()
+                    .getPlugin(JavaPluginConvention.class)
+                    .getSourceSets()
+                    .forEach(sourceSet -> copyTask.from(sourceSet.getRuntimeClasspath()));
+            Path tempDir = Path.of(
+                    Optional.ofNullable(System.getProperty("java.io.tmpdir")).orElse("/tmp"),
+                    UUID.randomUUID().toString()
+            );
             copyTask.into(tempDir);
-            copyTask.doLast(ignored -> project.delete(tempDir));
+            copyTask.doLast(ignored -> copyTask.getProject().delete(tempDir));
         });
     }
 
     private void addDependencies(@NotNull Project project) {
         project.getPluginManager().apply(LombokPlugin.class);
-        // Gradle plugin 'io.freefair.lombok' v6 deprecates 'generateLombokConfig' task:
-        // requireTask(project, "generateLombokConfig").setEnabled(false);
+        if (project.getTasks().getNames().contains(LOMBOK_CONFIG_TASK_NAME)) {
+            // Gradle plugin 'io.freefair.lombok' version 6 deprecates
+            // 'generateLombokConfig' task, so this block may not be needed
+            requireTask(project, LOMBOK_CONFIG_TASK_NAME).setEnabled(false);
+        }
     }
 }
--- src/main/java/com/github/decaland/touchstone/loadout/layers/flavors/KotlinLayer.java
+++ src/main/java/com/github/decaland/touchstone/loadout/layers/flavors/KotlinLayer.java
@@ -3,7 +3,14 @@ package com.github.decaland.touchstone.loadout.layers.flavors;
 import com.github.decaland.touchstone.loadout.Loadout;
 import com.github.decaland.touchstone.loadout.layers.ProjectAwareLayer;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.logging.LogLevel;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.internal.logging.services.DefaultLoggingManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.dokka.gradle.DokkaPlugin;
+import org.jetbrains.dokka.gradle.DokkaTask;
 import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension;
 import org.jetbrains.kotlin.allopen.gradle.AllOpenGradleSubplugin;
 import org.jetbrains.kotlin.allopen.gradle.SpringGradleSubplugin;
@@ -14,12 +21,22 @@ import org.jetbrains.kotlin.noarg.gradle.KotlinJpaSubplugin;
 import org.jetbrains.kotlinx.serialization.gradle.SerializationGradleSubplugin;
 import org.springframework.boot.gradle.plugin.SpringBootPlugin;
 
+import java.io.File;
 import java.util.Collections;
 
 import static com.github.decaland.touchstone.configs.BuildParametersManifest.VERSION_JAVA;
 import static com.github.decaland.touchstone.configs.BuildParametersManifest.VERSION_KOTLIN_API;
+import static org.gradle.api.plugins.internal.JvmPluginsHelper.configureDocumentationVariantWithArtifact;
+import static org.gradle.api.plugins.internal.JvmPluginsHelper.findJavaComponent;
 
 public class KotlinLayer extends ProjectAwareLayer {
+
+    private static final String DOKKA_HTML_TASK_NAME = "dokkaHtml";
+
+    private static final String KDOC = "kdoc";
+    private static final String KDOC_DIRECTORY_NAME = KDOC;
+    private static final String KDOC_JAR_TASK_NAME = KDOC + "Jar";
+    private static final String KDOC_ELEMENTS_CONFIGURATION_NAME = KDOC + "Elements";
 
     public KotlinLayer() {
     }
@@ -33,12 +50,14 @@ public class KotlinLayer extends ProjectAwareLayer {
         project.getPluginManager().apply(SerializationGradleSubplugin.class);
         project.getPluginManager().apply(AllOpenGradleSubplugin.class);
         project.getPluginManager().apply(KotlinJpaSubplugin.class);
+        project.getPluginManager().apply(DokkaPlugin.class);
     }
 
     @Override
-    public void configure(Project project, Loadout.Layers layers) {
+    public void configure(@NotNull Project project, Loadout.Layers layers) {
         project.getTasks().withType(KotlinCompile.class, this::configureKotlinPluginCompileTasks);
         openClassesForJpa(project);
+        withKdocJar(project);
         addDependencies(project);
     }
 
@@ -50,7 +69,7 @@ public class KotlinLayer extends ProjectAwareLayer {
         kotlinOptions.setFreeCompilerArgs(Collections.singletonList("-Xjsr305=strict"));
     }
 
-    private void openClassesForJpa(Project project) {
+    private void openClassesForJpa(@NotNull Project project) {
         AllOpenExtension allOpenExtension = project.getExtensions().findByType(AllOpenExtension.class);
         if (allOpenExtension != null) {
             allOpenExtension.annotations(
@@ -61,7 +80,36 @@ public class KotlinLayer extends ProjectAwareLayer {
         }
     }
 
-    private void addDependencies(Project project) {
+    private void withKdocJar(@NotNull Project project) {
+        // Set up convinience variables
+        TaskContainer tasks = project.getTasks();
+        ConfigurationContainer configurations = project.getConfigurations();
+        JavaPluginConvention convention = project.getConvention().getPlugin(JavaPluginConvention.class);
+        File kdocDir = new File(convention.getDocsDir(), KDOC_DIRECTORY_NAME);
+
+        // Set up dokka HTML task
+        tasks.named(DOKKA_HTML_TASK_NAME, DokkaTask.class, dokkaHtmlTask -> {
+            ((DefaultLoggingManager) dokkaHtmlTask.getLogging()).setLevelInternal(LogLevel.INFO);
+            dokkaHtmlTask.getOutputDirectory().set(kdocDir);
+        });
+
+        // Configure variant and jar task for kdoc; make kdoc jar depend on Dokka HTML
+        configureDocumentationVariantWithArtifact(
+                KDOC_ELEMENTS_CONFIGURATION_NAME,
+                null,
+                KDOC,
+                Collections.emptyList(),
+                KDOC_JAR_TASK_NAME,
+                kdocDir,
+                findJavaComponent(project.getComponents()),
+                configurations,
+                tasks,
+                project.getObjects()
+        );
+        tasks.named(KDOC_JAR_TASK_NAME, kdocJarTask -> kdocJarTask.dependsOn(tasks.named(DOKKA_HTML_TASK_NAME)));
+    }
+
+    private void addDependencies(@NotNull Project project) {
         project.getDependencies().add("implementation", "org.jetbrains.kotlin:kotlin-stdlib-jdk8");
         project.getDependencies().add("implementation", "org.jetbrains.kotlin:kotlin-reflect");
         project.getDependencies().add("implementation", "org.jetbrains.kotlinx:kotlinx-serialization-json");
