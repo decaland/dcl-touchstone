b827299a61a773342c9c3f3568660ed2ae2a9aed
Erik Sargazakov <erik@serpnet.org>
2021-08-04 13:47:38 +0300

    DCL-10: Introduce stubbed tasks

--- src/main/java/com/github/decaland/touchstone/loadout/layers/releasing/ReleaseFlowLayer.java
+++ src/main/java/com/github/decaland/touchstone/loadout/layers/releasing/ReleaseFlowLayer.java
@@ -4,14 +4,32 @@ import com.github.decaland.touchstone.loadout.Loadout;
 import com.github.decaland.touchstone.loadout.layers.ProjectAwareLayer;
 import org.gradle.api.Project;
 
+import java.util.Optional;
+
 public class ReleaseFlowLayer extends ProjectAwareLayer {
 
     @Override
     public void apply(Project project, Loadout.Layers layers) {
-        project.getTasks().register("release", taskConfig -> {
+        project.getTasks().register("prepareRelease", taskConfig -> {
+            taskConfig.setGroup("Release");
+            taskConfig.setDescription("Creates release commit");
             taskConfig.doLast(task -> {
-                task.getProject().getLogger().lifecycle("Releasing this version (not really)");
+                task.getLogger().lifecycle("[DEBUG] Preparing release");
             });
+        });
+        project.getTasks().register("release", taskConfig -> {
+            taskConfig.setGroup("Release");
+            taskConfig.setDescription("Lifecycle task");
+            taskConfig.dependsOn(project.getTasks().named("prepareRelease"));
+            Optional.ofNullable(project.getTasks().findByName("publish"))
+                    .map(taskConfig::dependsOn);
+            taskConfig.dependsOn(project.getTasks().named("build"));
+            taskConfig.doLast(task -> {
+                task.getProject().getLogger().lifecycle("[DEBUG] Releasing");
+            });
+        });
+        project.getTasks().named("assemble", classesTask -> {
+            classesTask.mustRunAfter(project.getTasks().named("prepareRelease"));
         });
     }
 }
